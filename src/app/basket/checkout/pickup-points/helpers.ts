6d9d687b5f5a1f73b162d269c1a23a5a324cfd93
dikobrazik <ilnarik@yandex-team.ru>
2024-05-29 13:07:33 +0500

    center drawer on desktop

--- src/app/basket/checkout/pickup-points/helpers.ts
+++ src/app/basket/checkout/pickup-points/helpers.ts
@@ -1,20 +1,14 @@
+import {Coordinate} from 'ol/coordinate';
 import Style from 'ol/style/Style';
 
 export const pickupPointCircleStyle = new Style({
     renderer(coordinates, state) {
-        const [[x, y], [x1, y1]] = coordinates;
+        const [[x, y], [x1, y1]] = coordinates as Coordinate[];
         const ctx = state.context;
         const dx = x1 - x;
         const dy = y1 - y;
         const radius = Math.sqrt(dx * dx + dy * dy);
 
-        const innerRadius = 0;
-        const outerRadius = radius * 1.4;
-
-        // const gradient = ctx.createRadialGradient(x, y, innerRadius, x, y, outerRadius);
-        // gradient.addColorStop(0, 'rgba(255,0,0,0)');
-        // gradient.addColorStop(0.6, 'rgba(255,0,0,0.2)');
-        // gradient.addColorStop(1, 'rgba(255,0,0,0.8)');
         ctx.beginPath();
         ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
         ctx.fillStyle = 'rgba(255,0,0,1)';
@@ -36,19 +30,12 @@ export const pickupPointCircleStyle = new Style({
 
 export const pickupPointCircleSelectStyle = new Style({
     renderer(coordinates, state) {
-        const [[x, y], [x1, y1]] = coordinates;
+        const [[x, y], [x1, y1]] = coordinates as Coordinate[];
         const ctx = state.context;
         const dx = x1 - x;
         const dy = y1 - y;
         const radius = Math.sqrt(dx * dx + dy * dy);
 
-        const innerRadius = 0;
-        const outerRadius = radius * 1.4;
-
-        // const gradient = ctx.createRadialGradient(x, y, innerRadius, x, y, outerRadius);
-        // gradient.addColorStop(0, 'rgba(255,0,0,0)');
-        // gradient.addColorStop(0.6, 'rgba(255,0,0,0.2)');
-        // gradient.addColorStop(1, 'rgba(255,0,0,0.8)');
         ctx.beginPath();
         ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
         ctx.fillStyle = 'rgba(185,0,0,1)';
