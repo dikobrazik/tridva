26977f4d503de1fda8416d247a075812e40024f6
dikobrazik <ilnar.khusaenov@gmail.com>
2024-10-31 11:14:23 +0300

    see also loading first level category offers

--- src/app/offers/[id]/page.tsx
+++ src/app/offers/[id]/page.tsx
@@ -168,7 +168,7 @@ export default async function Offer(props: Props) {
                 <Text size={16} weight={600}>
                     Смотрите также
                 </Text>
-                <SeeAlso categoryId={offer.categoryId} page={page} />
+                <SeeAlso categoryId={categoryAncestors[0].id} page={page} />
             </Block>
         </Column>
     );
