b0d8c18bea2c49d1e91ddd30f9e6903de16cd110
dikobrazik <ilnar.khusaenov@gmail.com>
2024-11-06 17:38:17 +0300

    favicons fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,16 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" type="image/png" href="/favicon/favicon-16x16.png" sizes="16x16" />
+                <link rel="icon" type="image/png" href="/favicon/favicon-32x32.png" sizes="32x32" />
+                <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+                <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+                <link rel="shortcut icon" href="/favicon/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+                <link rel="manifest" href="/favicon/site.webmanifest" />
+                <meta name="apple-mobile-web-app-title" content="Tridva" />
+            </head>
             <body className={classNames(inter.className, css.body)}>
                 <StoreProvider>
                     <AuthTokenProvider>
